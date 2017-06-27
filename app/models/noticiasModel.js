0585d08d91526fa45f6ea013adcf41b570dc908d
Adriano Boese <desenvacc@gmail.com>
2017-06-27 09:51:42 -0300

    Aula 40

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -1,12 +1,15 @@
-module.exports = function(){
+module.exports = function() {
 
-  this.getNoticias = function(connection, callback){
+  this.getNoticias = function(connection, callback) {
     connection.query('SELECT * FROM noticias', callback);
   };
 
-  this.getNoticia = function(connection, callback){
+  this.getNoticia = function(connection, callback) {
     connection.query('SELECT * FROM noticias WHERE id_noticia = 2', callback);
   };
 
+  this.salvarNoticia = function(noticia, connection, callback) {
+    connection.query('INSERT INTO noticias SET ?', noticia, callback);
+  };
   return this;
 }
